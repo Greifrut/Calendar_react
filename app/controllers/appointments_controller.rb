96021e6a375bc4aa4112eebd66f67eef3cce511a
Grefrut <artur.bunko@gmail.com>
2018-04-11 14:03:16 +0300

    edit

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,17 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointments = Appointment.order('apt_time ASC')
+    @appointment = Appointment.new
   end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    redirect_to root_path
+  end
+
+  private
+    def appointment_params
+      params.require(:appointment).permit(:title, :apt_time)
+    end
+    
 end
